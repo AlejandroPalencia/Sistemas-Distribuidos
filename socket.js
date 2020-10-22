c14f5b0703686a26df8f4428cff1a0f0d41ce491
AlejandroPalencia <alejandro.palenciap@estudiante.uam.es>
2020-10-22 19:28:05 +0200

    socket.js

---
+++ socket.js
@@ -0,0 +1,21 @@
+import socketIO from 'socket.io'
+
+let io = null
+
+export function startSocket(server){
+    io = socketIO(server)
+    io.on('connection', (socket) => {
+        console.log('a user connected')
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+    })
+}
+
+export function broadcastMsg(msg){
+    // enviamos msg a todos, con el tipo de evento 'msg'
+    if (io)
+        io.emit('msg', msg)
+    else console.error("Call startSocket(server) before sending messages")
+   }
+   
