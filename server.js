563fd7d5011a31dbef8f4068e6561c3ffaf649a5
AlejandroPalencia <alejandro.palenciap@estudiante.uam.es>
2020-10-24 19:48:50 +0200

    funciona correctamente, arreglado el tema del bodyParser

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import {startSocket, broadcastMsg} from './socket.js'
-import bodyParser from "body-parser"
+import bodyParser from 'body-parser'
 
 const PORT = 8080
 const app = express()
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({​​​​​​​​extended: false}​​​​​​​​))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send('Hello World I am running locally')})
