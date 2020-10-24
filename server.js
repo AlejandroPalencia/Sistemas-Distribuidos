5d52a1e88900d9b10ff2f01ea813116e45cceebb
AlejandroPalencia <alejandro.palenciap@estudiante.uam.es>
2020-10-24 19:43:09 +0200

    axios.put con {msg} e intento de implementacion config de express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import {startSocket, broadcastMsg} from './socket.js'
-import bodyParser from 'body-parser'
+import bodyParser from "body-parser"
 
 const PORT = 8080
 const app = express()
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({​​​​​​​​extended: false}​​​​​​​​))
 
